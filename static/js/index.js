5b7b16009986a9d0248b289a84b8bcb1d4b2532d
Nolan <kurylon@telus.net>
2019-07-07 17:45:53 -0700

    added imgs/content for index page

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,20 @@
-$(".chevron").click(function () { $('html, body').animate({ scrollTop: $(".chevron").offset().top }, 1000); });
+
+// Scrolling Effect
+$(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('black');
+    }
+
+    else {
+        $('nav').removeClass('black');
+    }
+})
+
+$(".intro-start").click(function() {
+  $("html, body").animate({ scrollTop: $("#card-1").offset().top - 100 }, 1000);
+});
+
+$(".intro-end").click(function() {
+  $("html, body").animate({ scrollTop: $("#project-directory").offset().top - 80 }, 1000);
+});
+
